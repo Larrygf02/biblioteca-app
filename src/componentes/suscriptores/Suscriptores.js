cefcdd8930c8c79ad71cb59a84144de59cccd499
larrygf02 <raulgf_02@hotmail.com>
2019-12-22 22:39:07 -0500

    Mostrando suscriptores

--- src/componentes/suscriptores/Suscriptores.js
+++ src/componentes/suscriptores/Suscriptores.js
@@ -1,11 +1,63 @@
 import React from 'react'
+import { compose } from 'redux'
+import { connect } from 'react-redux'
+import { firestoreConnect } from 'react-redux-firebase'
+import { Link } from 'react-router-dom'
 
-const Suscriptores = () => {
+import Spinner from '../layout/Spinner'
+
+
+const Suscriptores = ({suscriptores}) => {
+    if (!suscriptores) return <Spinner></Spinner>
     return (
-        <div>
-            <h1>Suscriptores</h1>
+        <div className="row">
+            <div className="col-md-12 mb-4">
+                <Link
+                    to="/suscriptores/nuevo"
+                    className="btn btn-primary">
+                        <i className="fas fa-plus"></i>
+                        {' '}
+                        Nuevo suscriptor
+                </Link>
+            </div>
+            <div className="col-md-8">
+                <h2>
+                    <i className="fas fa-users"></i> Suscriptores
+                </h2>
+            </div>
+            <table className="table table-striped mt-4">
+                <thead className="text-light bg-primary">
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Carrera</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {suscriptores.map(suscriptor => (
+                        <tr key={suscriptor.id}>
+                            <td>{suscriptor.nombre} {suscriptor.apellido}</td>
+                            <td>{suscriptor.carrera}</td>
+                            <td>
+                                <Link 
+                                to={`/suscriptores/mostrar/${suscriptor.id}`}
+                                className="btn btn-success btn-block">
+                                    <i className="fas fa-angle-double-right"> 
+                                    </i>{' '}
+                                        Mas información
+                                </Link>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     )
 }
 
-export default Suscriptores;
+export default compose(
+    firestoreConnect([{ collection: 'suscriptores' }]),
+    connect((state, props) => ({
+        suscriptores: state.firestore.ordered.suscriptores
+    }))
+) (Suscriptores);
