179e998a89ff2389ce3af8fc5dde551f2ccf773e
larrygf02 <raulgf_02@hotmail.com>
2019-12-23 22:19:38 -0500

    Mostrando un suscriptor

--- src/componentes/suscriptores/MostrarSuscriptor.js
+++ src/componentes/suscriptores/MostrarSuscriptor.js
@@ -1,4 +1,11 @@
 import React from 'react';
+import { compose } from 'redux'
+import { connect } from 'react-redux'
+import { firestoreConnect } from 'react-redux-firebase'
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+
+import Spinner from '../layout/Spinner'
 
 const MostrarSuscriptor = () => {
     return (
@@ -6,4 +13,15 @@ const MostrarSuscriptor = () => {
     )
 }
 
-export default MostrarSuscriptor;
+export default compose(
+    firestoreConnect(props => [
+        {
+            collection: 'suscriptores',
+            storeAs: 'suscriptor',
+            doc: props.match.params.id
+        }
+    ]),
+    connect(({firesore: { ordered }}, props) => ({
+        suscriptor: ordered.suscriptor && ordered.suscriptor[0]
+    }))
+)(MostrarSuscriptor)
