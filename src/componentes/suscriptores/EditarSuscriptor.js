5961c3fc05839807b7468d3be2c13cdb051422ac
larrygf02 <raulgf_02@hotmail.com>
2019-12-24 09:12:02 -0500

    Editar suscriptor

--- src/componentes/suscriptores/EditarSuscriptor.js
+++ src/componentes/suscriptores/EditarSuscriptor.js
@@ -1,13 +1,141 @@
 import React, { Component } from 'react';
+import { compose } from 'redux'
+import { connect } from 'react-redux'
+import { firestoreConnect } from 'react-redux-firebase'
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+
+import Spinner from '../layout/Spinner'
 
 class EditarSuscriptor extends Component {
-    state = {}
 
+    // crear los refs
+    nombreInput = React.createRef();
+    apellidoInput = React.createRef();
+    codigoInput = React.createRef();
+    carreraInput = React.createRef();
+
+    // editar el suscriptor en la bd
+    editarSuscriptor = e => {
+        e.preventDefault();
+
+        // crear el objeto que se va actualizar
+        const suscriptorActualizado = {
+            nombre: this.nombreInput.current.value,
+            apellido: this.apellidoInput.current.value,
+            carrera: this.carreraInput.current.value,
+            codigo: this.codigoInput.current.value
+        }
+
+        // almacenarlo en firestore
+        const { suscriptor, firestore, history } = this.props
+        firestore.update({
+            collection: 'suscriptores',
+            doc: suscriptor.id,
+        }, suscriptorActualizado).then(history.push('/suscriptores'))
+
+
+    }
     render() {
+
+        const { suscriptor } = this.props
+        if (!suscriptor) return <Spinner>s</Spinner>
         return (
-            <h1>Editar Suscriptor</h1>
+            <div className="row">
+            <div className="col-12 mb-4">
+                <Link to={'/suscriptores'} className="btn btn-secondary">
+                    <i className="fas fa-arrow-circle-left"></i>{''}
+                    Volver al listado
+                </Link>
+            </div>
+            <div className="col-12">
+                <h2>
+                    <i className="fas fa-user"></i> {''}
+                    Editar Suscriptor
+                </h2>
+                <div className="row justify-content-center">
+                    <div className="col-md-8 mt-5">
+                        <form
+                            onSubmit={this.editarSuscriptor}>
+                            <div className="form-group">
+                                <label>Nombre:</label>
+                                <input 
+                                type="text"
+                                className="form-control"
+                                name="nombre"
+                                placeholder="Nombre del suscriptor"
+                                required
+                                ref={this.nombreInput}
+                                defaultValue={suscriptor.nombre}
+                                />
+                            </div>
+
+                            <div className="form-group">
+                                <label>Apellido:</label>
+                                <input 
+                                type="text"
+                                className="form-control"
+                                name="apellido"
+                                placeholder="Apellido del suscriptor"
+                                required
+                                ref={this.apellidoInput}
+                                defaultValue={suscriptor.apellido}
+                                />
+                            </div>
+
+                            <div className="form-group">
+                                <label>Carrera:</label>
+                                <input 
+                                type="text"
+                                className="form-control"
+                                name="carrera"
+                                placeholder="Carrera del suscriptor"
+                                required
+                                ref={this.carreraInput}
+                                defaultValue={suscriptor.carrera}
+                                />
+                            </div>
+
+                            <div className="form-group">
+                                <label>Codigo:</label>
+                                <input 
+                                type="text"
+                                className="form-control"
+                                name="codigo"
+                                placeholder="Codigo del suscriptor"
+                                required
+                                ref={this.codigoInput}
+                                defaultValue={suscriptor.codigo}
+                                />
+                            </div>
+
+                            <input 
+                            type="submit"
+                            value="Editar Suscriptor"
+                            className="btn btn-success"/>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
         )
     }
 }
 
-export default EditarSuscriptor;
+EditarSuscriptor.propTypes = {
+    firestore: PropTypes.object.isRequired
+}
+
+export default compose(
+    firestoreConnect(props => [
+        {
+            collection: 'suscriptores',
+            storeAs: 'suscriptor',
+            doc: props.match.params.id
+        }
+    ]),
+    connect(({firestore: { ordered }}, props) => ({
+        suscriptor: ordered.suscriptor && ordered.suscriptor[0]
+    }))
+)(EditarSuscriptor)
